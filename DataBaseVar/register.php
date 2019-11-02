16e3a97fd6b4f6163acb7251ac1ad2db86eb26e8
OtsukiTaisuke <taisuke.march4@icloud.com>
2019-11-02 15:14:08 +0900

    okar

--- DataBaseVar/register.php
+++ DataBaseVar/register.php
@@ -45,10 +45,10 @@
  <body>
  <header>
    <div>
-     <a href="/LoginBoard/DataBaseVar/index.php">TOP</a>
-     <a href="/LoginBoard/DataBaseVar/register.php">新規作成</a>
-     <a href="/LoginBoard/DataBaseVar/login.php">ログイン</a>
-     <a href="/LoginBoard/DataBaseVar/logout.php">ログアウト</a>
+     <a href="/home/ec2-user/LoginBoard/DataBaseVar/index.php">TOP</a>
+     <a href="/home/ec2-user/LoginBoard/DataBaseVar/register.php">新規作成</a>
+     <a href="/home/ec2-user/LoginBoard/DataBaseVar/login.php">ログイン</a>
+     <a href="/home/ec2-user/LoginBoard/DataBaseVar/logout.php">ログアウト</a>
    </div>
    <h1>新規作成</h1>
  </header>
